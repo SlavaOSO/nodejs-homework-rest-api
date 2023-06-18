551a0b713416d7f7ba00dc4c445427a2beb8a65b
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2023-06-18 17:02:52 +0300

    Update contacts.js
    Виправив меседж у name та phone.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -13,10 +13,10 @@ const contactAddSchema = Joi.object({
     .messages({ "any.required": `missing required name field` }),
   email: Joi.string()
     .required()
-    .messages({ "any.required": `missing required name field` }),
+    .messages({ "any.required": `missing required email field` }),
   phone: Joi.string()
     .required()
-    .messages({ "any.required": `missing required name field` }),
+    .messages({ "any.required": `missing required phone field` }),
 });
 
 router.get("/", async (req, res, next) => {
